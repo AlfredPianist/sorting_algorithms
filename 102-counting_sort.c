47b5630ba9cb53a40cdd139b0d4f0ea6bfe57a48
Sebastian Asus Elementary <sebas93cay@gmail.com>
2021-06-12 03:17:38 +0000

    Counting sort implementation

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,25 +10,24 @@ void counting_sort(int *array, size_t size)
 	int max = 0, i, total = 0;
 	int *count_array = NULL, *copy_array = NULL;
 
+	if (array == NULL)
+		return;
+	for (i = 0; i < (int)size; i++)
+		if (array[i] > max)
+			max = array[i];
+
 	count_array = malloc(sizeof(int) * (max + 1));
 	if (count_array == NULL)
 		exit(1);
 	copy_array = malloc(sizeof(int) * size);
 	if (copy_array == NULL)
 		exit(1);
-	for (i = 0; i < (int)size; i++)
-		copy_array[i] = array[i];
-
-	print_array(array, size);
-	print_array(copy_array, size);
-
-	for (i = 0; i < (int)size; i++)
-		if (array[i] > max)
-			max = array[i];
 
 	for (i = 0; i <= max; i++)
 		count_array[i] = 0;
 
+	for (i = 0; i < (int)size; i++)
+		copy_array[i] = array[i];
 	for (i = 0; i < (int)size; i++)
 		count_array[array[i]]++;
 
@@ -37,9 +36,8 @@ void counting_sort(int *array, size_t size)
 		count_array[i] += total;
 		total = count_array[i];
 	}
+
 	print_array(count_array, max + 1);
-	print_array(array, size);
-	print_array(copy_array, size);
 
 	for (i = 0; i < (int)size; i++)
 	{
